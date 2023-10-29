6ceace95b3f7a71031af732ddeff11537ddccbc4
orenatonunes <renatodsnunes@gmail.com>
2023-10-29 19:42:59 -0300

    03

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -12,3 +12,28 @@ print(numeros)
 
 carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
 print(carro)
+
+#------------------------------------------------------------------->
+
+#  FILTRO VERSÃO 1
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = []
+
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+print(pares)
+
+#------------------------------------------------------------------->
+
+#  FILTRO VERSÃO 2
+numeos = [1, 30, 21, 2, 9, 65, 34]
+pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
+
+numeos = [1, 30, 21, 2, 9, 65, 34]
+quadrado = [numero ** 2 for numero in numeros]
+print(quadrado)
+
+
